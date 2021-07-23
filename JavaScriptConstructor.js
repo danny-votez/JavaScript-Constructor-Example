e70e3877c0efc7d5897c686e056307c9c06db457
Danny-Votez <77758884+danny-votez@users.noreply.github.com>
2021-07-23 15:51:41 +0300

    Add files via upload

---
+++ JavaScriptConstructor.js
@@ -0,0 +1,43 @@
+// In JavaScript, object constructors are impotant
+
+function Elizabeth (name, profession, age, siblings, education){
+    // In this case, Elizabeth forms the Object Constructor
+    // As good practice, starting Object construtors with first upper case letter is encouraged
+    this.name = name;
+    this.profession = profession;
+    this.age = age;
+    this.siblings = siblings;
+    this.education = education;
+}
+
+// creating a new object now
+// This is done through calling the previously created constructor element, with the 'new' keyword
+
+// it is good practice to create objects using 'const' keyword
+const herInfo = new Elizabeth('Elizabeth The Queen', 'Designer', 24, 2, 'Mcensal Design School - Nairobi');
+
+// We can now access the individual elements using the following statements
+console.log(herInfo.name);
+console.log(herInfo.education);
+console.log(herInfo.profession);
+
+
+// For accessing all the information, we then adopt the following command
+console.log(herInfo);
+
+
+/* =========RESULTS========
+
+
+Elizabeth The Queen
+Mcensal Design School - Nairobi
+Designer
+Elizabeth {
+  name: 'Elizabeth The Queen',
+  profession: 'Designer',
+  age: 24,
+  siblings: 2,
+  education: 'Mcensal Design School - Nairobi'
+}
+
+*/
